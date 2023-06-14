567d25479f6afc9e69c19b46004fbcdefd2332de
dhuziran <danielhuziran@gmail.com>
2023-06-14 13:12:56 -0500

    update 4

--- src/components/single-post/SinglePost.jsx
+++ src/components/single-post/SinglePost.jsx
@@ -1,4 +1,23 @@
+import { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import { fetchPosts } from '../../API';
+
 export default function SinglePost(){
+
+    const [posts, setPosts] = useState([]);
+    useEffect(() => {
+        const getPosts = async () => {
+          try {
+            const data = await fetchPosts();
+            setPosts(data);
+          } catch (error) {
+            console.log(error);
+          }
+        };
+        getPosts();
+      }, []);
+
+    let { id } = useParams()
     return (
         <h1>hello world</h1>
     )
