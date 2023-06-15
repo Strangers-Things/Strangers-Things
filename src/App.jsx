099b7d546a6fb9f06c8b96ee5d146aa6b7e45406
dhuziran <danielhuziran@gmail.com>
2023-06-15 13:44:29 -0500

    update 5

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,63 @@
-import { useState, useEffect } from 'react'
-import { Routes, Route } from 'react-router-dom'
-import { fetchPosts } from './API'
-import Home from './components/home/Home'
-import Nav from './components/navbar/Nav'
-import Posts from './components/post/Posts'
-import SinglePost from './components/single-post/SinglePost'
+import { useState, useEffect } from "react";
+import { Routes, Route } from "react-router-dom";
+import { fetchPosts } from "./API";
+import Home from "./components/home/Home";
+import Nav from "./components/navbar/Nav";
+import Posts from "./components/post/Posts";
+import SinglePost from "./components/single-post/SinglePost";
 // import Login from './components/login/Login'
-import Register from './components/register/Register'
-import CreatePost from './components/create-post/CreatePost'
-import './App.css'
+import Register from "./components/register/Register";
+import Profile from "./components/profile/Profile";
+import CreatePost from "./components/create-post/CreatePost";
+import "./App.css";
 
 function App() {
-  const [loggedIn, setLoggedIn] = useState(false)
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [posts, setPosts] = useState([]);
 
   useEffect(() => {
-    const token = localStorage.getItem('token')
+    const getPosts = async () => {
+      try {
+        const data = await fetchPosts();
+        console.log(data);
+        setPosts(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getPosts();
+  }, []);
 
-    if(token) {
-      setLoggedIn(true)
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+
+    if (token) {
+      setLoggedIn(true);
     }
-  })
+  }, []);
   return (
     <div>
-      <Nav loggedIn = {loggedIn}/>
-    
+      <Nav loggedIn={loggedIn} />
 
       <Routes>
-        <Route path='/' element = {<Home />} />
-        <Route path='/posts' element = {<Posts />} />
-        <Route path = '/posts/:id' element = {<SinglePost />} />
+        <Route path="/" element={<Home />} />
+        <Route
+          path="/posts"
+          element={<Posts posts={posts} setPosts={setPosts} />}
+        />
+        <Route
+          path="/posts/:id"
+          element={<SinglePost posts={posts} setPosts={setPosts} />}
+        />
         {/* <Route path='/login' element = {<Login />} /> */}
-        <Route path='/register' element = {<Register loggedIn = {loggedIn} setLoggedIn = {setLoggedIn}/>} />
+        <Route
+          path="/register"
+          element={<Register loggedIn={loggedIn} setLoggedIn={setLoggedIn} />}
+        />
+        <Route path="/profile" element={<Profile />} />
       </Routes>
     </div>
-
-  )
+  );
 }
 
-export default App
+export default App;
